b8cc7edabe33788e5ba354f9e3f0e3ef31dfede7
udenisilva <ukui.silva@gmail.com>
2024-08-09 01:24:05 +0530

    add fragments

--- app/src/main/java/com/udeni/e_commerce_system_development_task/MainActivity.java
+++ app/src/main/java/com/udeni/e_commerce_system_development_task/MainActivity.java
@@ -1,12 +1,14 @@
 package com.udeni.e_commerce_system_development_task;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Fragment fragment_1 = new ViewOrderListFragment();
+        Fragment fragment_2 = new ViewOrderFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragment, fragment_1).commit();
+
+        Button btn_fragment = findViewById(R.id.btn_fragment);
+        btn_fragment.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragment, fragment_2).addToBackStack(null).commit();
         });
     }
 }
---
+++ app/src/main/java/com/udeni/e_commerce_system_development_task/ViewOrderFragment.java
@@ -0,0 +1,28 @@
+package com.udeni.e_commerce_system_development_task;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ViewOrderFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ViewOrderFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_order, container, false);
+    }
+}
