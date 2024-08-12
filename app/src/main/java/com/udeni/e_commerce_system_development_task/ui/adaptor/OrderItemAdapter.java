f60bfcfc662643ebbed28fbae7b989f3b1067126
udenisilva <ukui.silva@gmail.com>
2024-08-12 17:36:10 +0530

    develop search

--- app/src/main/java/com/udeni/e_commerce_system_development_task/ui/adaptor/OrderItemAdapter.java
+++ app/src/main/java/com/udeni/e_commerce_system_development_task/ui/adaptor/OrderItemAdapter.java
@@ -1,6 +1,5 @@
 package com.udeni.e_commerce_system_development_task.ui.adaptor;
 
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,15 +10,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.udeni.e_commerce_system_development_task.R;
 import com.udeni.e_commerce_system_development_task.data.local.model.Order;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderItemAdapter extends RecyclerView.Adapter<OrderItemAdapter.ViewHolder> {
 
     private List<Order> localDataSet;
+    private List<Order> orderListResult;
     private OnItemClickListener listener;
 
     public OrderItemAdapter(List<Order> dataSet) {
-        localDataSet = dataSet;
+        localDataSet = new ArrayList<>(dataSet);
+        orderListResult = dataSet;
     }
 
     @Override
@@ -32,7 +34,7 @@ public class OrderItemAdapter extends RecyclerView.Adapter<OrderItemAdapter.View
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-        Order order = localDataSet.get(position);
+        Order order = orderListResult.get(position);
         viewHolder.getTvOrderNumber().setText("Order #" + order.getOrderNumber());
         viewHolder.getTvCustomerName().setText(order.getCustomer().getName());
         viewHolder.getTvDateTime().setText(order.getDateTime());
@@ -47,9 +49,27 @@ public class OrderItemAdapter extends RecyclerView.Adapter<OrderItemAdapter.View
         });
     }
 
+    // Filter logic for search
+    public void filter(String text) {
+        orderListResult.clear();
+        if (text.isEmpty()) {
+            orderListResult.addAll(localDataSet);
+        } else {
+            text = text.toLowerCase();
+            for (Order order : localDataSet) {
+                if (order.getOrderNumber().contains(text) ||
+                        order.getCustomer().getName().toLowerCase().contains(text) ||
+                        String.valueOf(order.getCustomer().getCustomerId()).contains(text)) {
+                    orderListResult.add(order);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+
     @Override
     public int getItemCount() {
-        return localDataSet.size();
+        return orderListResult.size();
     }
 
     public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
